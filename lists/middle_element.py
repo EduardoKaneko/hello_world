435007e4f57e59dced14cdce5aff6df0338f816e
EduardoKaneko <eduardockaneko@gmail.com>
2019-06-22 12:15:03 -0300

    ex: append_sum

--- lists/middle_element.py
+++ lists/middle_element.py
@@ -1,11 +1,20 @@
+'''
+Create a function called middle_element that has one parameter named lst.
+
+If there are an odd number of elements in lst, the function should return the middle element. If there are an even number of elements, the function should return the average of the middle two elements.
+'''
+
 def middle_element(lst):
+  
     if len(lst)%2 == 0:
-        e1 = (len(lst)/2) - 1
-        e2  = len(lst)/2
+        e1 = int((len(lst)/2) - 1)
+        e2 = int(len(lst)/2)
         median = (lst[e1] + lst[e2])/2
+    
     elif len(lst)%2 != 0:
         e1 = int(len(lst)/2)
         median = lst[e1]
+    
     return median
 
-print(middle_element([5, 2, -10, -4, 4]))
+print(middle_element([5, 2, -10, -4, 4, 5]))
